72c4aecbd2647cd64d71d7ade1e76f160eadc13e
Gerald4012 <geraldbishop@hotmail.com>
2024-04-26 17:54:10 +0100

    comments added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 
+//if using default values they must be set in the declaration if a declaration is used
+int volume(int length=100, int width=20, int height=3);
+
 int main() {
-	
+	std::cout << volume() << std::endl;
+}
+
+//don't repeat or restate the default values in the definition
+int volume(int length, int width, int height) {
+	return length * width * height;
 }
